a9b1e08c42fbf81fac7ecb9941e16f463b0d99b9
Shane Hingtgen <shanehingtgen@gmail.com>
2024-04-09 20:53:41 -0500

    backing up api fetch for frontend - will need to fix image

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
